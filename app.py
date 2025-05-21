4d491f92cc766a7f2e5cfd8e1543d6ab1b47f75e
Merely  Tumbalobos <48566091+Merely22@users.noreply.github.com>
2025-05-21 11:09:29 -0500

    final version of main code

--- app.py
+++ app.py
@@ -2,17 +2,24 @@ import streamlit as st
 from src.load_file import cargar_csv, validar_dataframes
 from src.statistics import calcular_estadisticas
 from src.plots import generar_graficos, mostrar_resultados
+from PIL import Image
+import os
+#===============================================================
 
 def main():
-    st.set_page_config(page_title="Evaluación ISO 17123-8")
-    st.title("📡 Evaluación ISO 17123-8 para equipos GNSS")
+    st.set_page_config(page_title="Evaluación ISO 17123-8", layout= "centered")
+    st.title("Evaluación ISO 17123-8 para equipos GNSS")
     
     with st.sidebar:
         st.header("Parámetros de entrada")
-        serie = st.text_input("Número de serie del equipo")
-        nro_rovers = st.number_input("Cantidad de rovers", min_value=1, value=1)
-        sigma_fab_xy = st.number_input("Precisión de fábrica horizontal (σ)", value=0.01)
-        sigma_fab_z = st.number_input("Precisión de fábrica vertical (σ)", value=0.015)
+        serie = st.text_input("Número de serie del equipo", placeholder="PEX5-...")
+        nro_rovers = st.number_input("Cantidad de rovers", min_value=1)
+        sigma_fab_xy = st.number_input("Precisión de fábrica horizontal", value=0.007,
+                              step=0.001,
+                              format="%.3f")
+        sigma_fab_z = st.number_input("Precisión de fábrica vertical", value=0.012,
+                              step=0.001,
+                              format="%.3f")
         
         st.header("Carga de archivos")
         archivo_pa01 = st.file_uploader("Archivo CSV de PA01", type="csv")
@@ -29,8 +36,23 @@ def main():
             mostrar_resultados(resultados, serie)
             
             st.divider()
-            st.subheader("📈 Distribución de mediciones")
-            generar_graficos(df1, df2)
+            generar_graficos(df1,df2)
 
 if __name__ == "__main__":
     main()
+
+#===============================================================
+# Insert logos
+
+col1, col2 = st.columns(2,vertical_alignment="bottom" )
+
+with col1:
+    imagen_izq = Image.open("files/metta_logo_hd.png") 
+    st.logo(
+    imagen_izq)
+        
+with col2:
+    imagen_der = Image.open("files/Shabby stamp(bonus).png")
+    st.image(imagen_der,
+            width=170)
+
